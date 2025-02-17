6d32a3e3fa172481e7d04dd320d67ed8b7739328
joat21 <eridel61@yandex.ru>
2025-02-17 18:15:46 +0500

    init, css, fonts & images, layouts & pages, navigating

---
+++ app/layout.tsx
@@ -0,0 +1,15 @@
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+import clsx from 'clsx';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={clsx('antialiased', inter.className)}>{children}</body>
+    </html>
+  );
+}
